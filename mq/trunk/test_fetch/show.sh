8e93ce7cc4249fc6fa9c2340547fc9fd8ae98e7f
cwinux <cwinux@da00a48a-4684-a967-d7f9-89c093a7ad30>
2011-05-03 05:31:05 +0000

    git-svn-id: http://cwinux-mq.googlecode.com/svn/trunk@119 da00a48a-4684-a967-d7f9-89c093a7ad30

--- mq/trunk/test_fetch/show.sh
+++ mq/trunk/test_fetch/show.sh
@@ -1,6 +1,6 @@
-head -15 log/cwx_dispatch_test_01.log
-tail log/cwx_dispatch_test_01.log
-rm -f log/cwx_dispatch_test_01.log
-head -15 test1/log/cwx_dispatch_test_01.log
-tail test1/log/cwx_dispatch_test_01.log
-rm -f test1/log/cwx_dispatch_test_01.log
+head -15 log/cwx_mq_fetch_01.log
+tail log/cwx_mq_fetch_01.log
+rm -f log/cwx_mq_fetch_01.log
+head -15 fetch1/log/cwx_mq_fetch_01.log
+tail fetch1/log/cwx_mq_fetch_01.log
+rm -f fetch1/log/cwx_mq_fetch_01.log
