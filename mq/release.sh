0d2942ed843d81123da290a2e0eadd4ca03eb1ca
cwinux <cwinux@da00a48a-4684-a967-d7f9-89c093a7ad30>
2011-05-04 03:24:49 +0000

    git-svn-id: http://cwinux-mq.googlecode.com/svn/trunk@132 da00a48a-4684-a967-d7f9-89c093a7ad30

--- mq/release.sh
+++ mq/release.sh
@@ -1,3 +1,3 @@
 svn export https://cwinux-mq.googlecode.com/svn/trunk/mq/release/mq2.0 cwx_mq2.0
 cd cwx_mq2.0
-export https://cwinux-mq.googlecode.com/svn/trunk/mq/doc doc
+svn export https://cwinux-mq.googlecode.com/svn/trunk/mq/doc doc
