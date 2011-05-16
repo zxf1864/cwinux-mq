f17f768af154a471c71cf7b143badb7a87358d93
cwinux <cwinux@da00a48a-4684-a967-d7f9-89c093a7ad30>
2011-05-16 08:20:29 +0000

    git-svn-id: http://cwinux-mq.googlecode.com/svn/trunk@153 da00a48a-4684-a967-d7f9-89c093a7ad30

--- mq/release.sh
+++
@@ -1,3 +0,0 @@
-svn export https://cwinux-mq.googlecode.com/svn/trunk/mq/release/mq2.0 cwx_mq2.0
-cd cwx_mq2.0
-svn export https://cwinux-mq.googlecode.com/svn/trunk/mq/doc doc
--- mq/src/mq/stop.sh
+++
@@ -1,8 +0,0 @@
-cd slave
-./cwx_dispatch -stop
-cd ..
-./cwx_dispatch -stop
-
-rm  /data1/dispatch_data/data/binlog/*
-rm  /data3/dispatch_data/data/binlog/*
-
