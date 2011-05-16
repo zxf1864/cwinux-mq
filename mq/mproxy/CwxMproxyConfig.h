00fd51bf548842032d3ce0b7221fce23376fb142
cwinux <cwinux@da00a48a-4684-a967-d7f9-89c093a7ad30>
2011-05-16 08:32:06 +0000

    git-svn-id: http://cwinux-mq.googlecode.com/svn/trunk@156 da00a48a-4684-a967-d7f9-89c093a7ad30

---
+++ mq/mproxy/CwxMproxyConfig.h
@@ -0,0 +1,55 @@
+#ifndef __CWX_MQ_PROXY_CONFIG_H__
+#define __CWX_MQ_PROXY_CONFIG_H__
+/*
+版权声明：
+    本软件遵循GNU GPL V3（http://www.gnu.org/licenses/gpl.html），
+    联系方式：email:cwinux@gmail.com；微博:http://t.sina.com.cn/cwinux
+*/
+
+
+#include "CwxHostInfo.h"
+#include "CwxCommon.h"
+#include "CwxXmlFileConfigParser.h"
+#include "CwxMqDef.h"
+
+CWINUX_USING_NAMESPACE
+
+///echo压力测试的配置文件加载对象
+class CwxMproxyConfig
+{
+public:
+    CwxMproxyConfig()
+    {
+        m_uiTimeout = 5000;
+        m_szErrMsg[0] = 0x00;
+    }
+    
+    ~CwxMproxyConfig()
+    {
+    }
+public:
+    //加载配置文件.-1:failure, 0:success
+    int loadConfig(string const & strConfFile);
+    //输出配置文件
+    void outputConfig();
+    //获取加载配置文件的失败错误信息
+    char const* getError() { return m_szErrMsg; };
+private:
+    bool fetchHost(CwxXmlFileConfigParser& parser,
+        string const& path,
+        CwxHostInfo& host);
+    bool loadGroup(string const& path, CwxXmlTreeNode const* pGroup, CwxMqConfigQueue& group);
+    bool parseIds(string const& group, list<pair<CWX_UINT32, CWX_UINT32> >& ids);
+
+public:
+    string               m_strWorkDir;///<工作目录
+    CWX_UINT32           m_uiTimeout; ///<查询超时时间，单位为ms
+    CwxHostInfo          m_recv;      ///<代理消息接受的监听地址
+    map<CwxMqIdRange, CwxMqConfigQueue>  m_groupPasswd; ///<口令
+    map<CwxMqIdRange, string>    m_allowGroup; ///<允许的group，若不为空，则group必须在allow中存在，否则查deny
+    map<CwxMqIdRange, string>    m_denyGroup; ///<禁止的group，若allow为空，则查deny。若在deny中存在，则禁止。
+    CwxHostInfo          m_mq; ///<mq的服务器
+    char                 m_szErrMsg[2048];///<错误消息buf
+};
+
+#endif
---
+++ mq/mproxy/CwxMproxyRecvHandler.h
@@ -0,0 +1,45 @@
+#ifndef __CWX_MPROXY_RECV_HANDLER_H__
+#define __CWX_MPROXY_RECV_HANDLER_H__
+/*
+版权声明：
+    本软件遵循GNU GPL V3（http://www.gnu.org/licenses/gpl.html），
+    联系方式：email:cwinux@gmail.com；微博:http://t.sina.com.cn/cwinux
+*/
+#include "CwxCommander.h"
+#include "CwxTss.h"
+#include "CwxGlobalMacro.h"
+#include "CwxMqTss.h"
+
+CWINUX_USING_NAMESPACE
+
+class CwxMproxyApp;
+///代理mq消息的接受handle
+class CwxMproxyRecvHandler : public CwxCmdOp 
+{
+public:
+    ///构造函数
+    CwxMproxyRecvHandler(CwxMproxyApp* pApp):m_pApp(pApp)
+    {
+    }
+    ///析构函数
+    virtual ~CwxMproxyRecvHandler()
+    {
+    }
+public:
+    ///处理mq消息的函数
+    virtual int onRecvMsg(CwxMsgBlock*& msg,  CwxTss* pThrEnv);
+    //处理连接关闭的消息
+    virtual int onConnClosed(CwxMsgBlock*& msg, CwxTss* pThrEnv);
+    //超时坚持的消息
+    virtual int onTimeoutCheck(CwxMsgBlock*& msg, CwxTss* pThrEnv);
+public:
+    //回复代理的mq消息
+    static void reply(CwxMproxyApp* app, CwxMsgBlock* msg, CWX_UINT32 uiConnId);
+
+private:
+    CWX_UINT32 isAuth(CwxMqTss* pTss, CWX_UINT32 uiGroup, char const* szUser, char const* szPasswd);
+private:
+    CwxMproxyApp*     m_pApp;  ///<app对象
+};
+
+#endif 
