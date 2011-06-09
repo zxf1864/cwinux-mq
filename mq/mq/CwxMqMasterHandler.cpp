1b97bfd178f4d6b5effe79b5960686ef851a24f9
cwinux <cwinux@da00a48a-4684-a967-d7f9-89c093a7ad30>
2011-06-09 05:39:34 +0000

    git-svn-id: http://cwinux-mq.googlecode.com/svn/trunk@334 da00a48a-4684-a967-d7f9-89c093a7ad30

--- mq/mq/CwxMqMasterHandler.cpp
+++ mq/mq/CwxMqMasterHandler.cpp
@@ -99,7 +99,7 @@ int CwxMqMasterHandler::onRecvMsg(CwxMsgBlock*& msg, CwxTss* pThrEnv)
             }
             ulUnzipLen = m_uiBufLen;
             //解压
-            if (CwxZlib::unzip(m_unzipBuf, ulUnzipLen, (const unsigned char*)msg->rd_ptr(), msg->length()))
+            if (!CwxZlib::unzip(m_unzipBuf, ulUnzipLen, (const unsigned char*)msg->rd_ptr(), msg->length()))
             {
                 CWX_ERROR(("Failure to unzip recv msg, msg size:%u, buf size:%u", msg->length(), m_uiBufLen));
                 m_pApp->noticeReconnect(m_uiConnId, 2000); ///延时2秒钟重连
