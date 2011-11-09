256c89db787cc3587a471d75c568456fe1460647
cwinux <cwinux@da00a48a-4684-a967-d7f9-89c093a7ad30>
2011-11-09 06:54:09 +0000

    git-svn-id: http://cwinux-mq.googlecode.com/svn/trunk@519 da00a48a-4684-a967-d7f9-89c093a7ad30

--- mq/zk/zk_get.cpp
+++ mq/zk/zk_get.cpp
@@ -107,9 +107,9 @@ int main(int argc ,char** argv)
 		printf("data:%s\n", szBuf);
 		printf("czxid:%s\n", CwxCommon::toString(stat.czxid, szTmp, 10));
 		printf("mzxid:%s\n", CwxCommon::toString(stat.mzxid, szTmp, 10));
-		timestamp = stat.ctime/1000000;
+		timestamp = stat.ctime;
 		printf("ctime:%d %s", (int)stat.ctime%1000000, ctime(&timestamp));
-		timestamp = stat.mtime/1000000;
+		timestamp = stat.mtime;
 		printf("mtime:%d %s", (int)stat.mtime%1000000, ctime(&timestamp));
 		printf("version:%d\n", stat.version);
 		printf("cversion:%d\n", stat.cversion);
