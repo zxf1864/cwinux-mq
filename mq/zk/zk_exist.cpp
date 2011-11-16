8325de31f7f21c8133163d00ef880107b497f27c
cwinux <cwinux@da00a48a-4684-a967-d7f9-89c093a7ad30>
2011-11-16 15:12:12 +0000

    git-svn-id: http://cwinux-mq.googlecode.com/svn/trunk@602 da00a48a-4684-a967-d7f9-89c093a7ad30

--- mq/zk/zk_exist.cpp
+++ mq/zk/zk_exist.cpp
@@ -88,7 +88,7 @@ int parseArg(int argc, char**argv)
         printf("No host, set by -H\n");
         return -1;
     }
-	if (!g_node.begin() == g_node.end())
+	if (g_node.begin() == g_node.end())
 	{
 		if (g_strFile.length())
 		{
@@ -164,7 +164,7 @@ int main(int argc ,char** argv)
 			}
 		}
 
-		output(outFd, 0, 0, "node:  \nmsg:  success\n\node:\n", );
+		output(outFd, 0, 0, "node:  \nmsg:  success\n\node:\n");
 		struct Stat stat;
 		list<string>::iterator iter = g_node.begin();
 		while(iter != g_node.end()){
