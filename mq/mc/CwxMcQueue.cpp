de7208bd1069936d49f0c83ebe4edbf1be63863d
cwinux <cwinux@da00a48a-4684-a967-d7f9-89c093a7ad30>
2012-10-03 23:24:54 +0000

    git-svn-id: http://cwinux-mq.googlecode.com/svn/trunk@729 da00a48a-4684-a967-d7f9-89c093a7ad30

---
+++ mq/mc/CwxMcQueue.cpp
@@ -0,0 +1,66 @@
+#include "CwxMcQueue.h"
+///创建对象
+CwxMcQueueItem* CwxMcQueueItem::createItem(CWX_UINT64 ullSid,
+                                           CWX_UINT32 uiLogHostId, 
+                                           CWX_UINT32 uiLogTimestamp,
+                                           char const* szData,
+                                           CWX_UINT32 uiDataLen)
+{
+    CWX_UINT32 uiSize = sizeof(CwxMcQueueItem) + uiDataLen;
+    uiSize = (uiSize + ALIGN_SIZE - 1)/ALIGN_SIZE;
+    uiSize *= ALIGN_SIZE;
+    CwxMcQueueItem* item = (CwxMcQueueItem*)malloc(uiSize);
+    item->m_uiItemSize = uiSize;
+    item->m_uiDataSize = uiDataLen;
+    item->m_ullLogSid - ullSid;
+    item->m_uiLogHostId = uiLogHostId;
+    item->m_uiLogTimestamp = uiLogTimestamp;
+    if (uiDataLen) memcpy(item->m_szLogData, szData, uiDataLen);
+    return item;
+}
+void CwxMcQueueItem::destoryItem(CwxMcQueueItem* item){
+    free(item);
+}
+
+///添加数据
+void CwxMcQueue::push(CwxMcQueueItem* log){
+    CwxMutexGuard<CwxMutexLock>  lock(&m_lock);
+    m_queue.insert(log);
+    m_ullCurSize += log->getItemSize();
+    while(m_ullCurSize >  m_ullMaxSize){
+        log = *m_queue.begin();
+        m_ullCurSize -= log->getItemSize();
+        m_ullDiscardNum ++;
+        queue.erase(m_queue.begin());
+        CwxMcQueueItem::destoryItem(log);
+    }
+}
+///pop数据，空表示没有数据
+CwxMcQueueItem* CwxMcQueue::pop(){
+    CwxMutexGuard<CwxMutexLock>  lock(&m_lock);
+    if (m_queue.size()){
+        CwxMcQueueItem* log = *m_queue.begin();
+        m_ullCurSize -= log->getItemSize();
+        queue.erase(m_queue.begin());
+        return log;
+    }
+    return NULL;
+
+}
+///检查超时
+void CwxMcQueue::checkTimeout(CWX_UINT32 uiTime){
+    CwxMcQueueItem* log;
+    CwxMutexGuard<CwxMutexLock>  lock(&m_lock);
+    if (m_queue.size()){
+        log = *m_queue->rbegin();
+        CWX_UINT32 uiTimeoutTime = log->getLogTimestamp();
+        if (uiTimeoutTime > uiTime) uiTimeoutTime = uiTime;
+        while(true){
+            log = *m_queue->begin();
+            if (log->getLogTimestamp() + m_uiTimeout >= uiTimeoutTime) break;
+            m_queue->erase(m_queue->begin());
+            m_ullCurSize -= log->getItemSize();
+            CwxMcQueueItem::destoryItem(log);
+        }
+    }
+}
---
+++ mq/mc/CwxMcQueue.h
@@ -0,0 +1,128 @@
+#ifndef __CWX_MC_QUEUE_H__
+#define __CWX_MC_QUEUE_H__
+
+#include "CwxMqMacro.h"
+#include "CwxGlobalMacro.h"
+#include "CwxCommon.h"
+#include "CwxStl.h"
+#include "CwxStlFunc.h"
+#include "CwxMutexLock.h"
+#include "CwxLockGuard.h"
+
+// 队列的数据项
+struct CwxMcQueueItem{
+public:
+    enum{
+        ALIGN_SIZE = 128  ///< 数据空间按照128对齐
+    };
+public:
+    inline CWX_UINT64 getLogSid() const {
+        return m_ullLogSid;
+    }
+    inline CWX_UINT32 getLogHostId() const {
+        return m_uiLogHostId;
+    }
+    inline CWX_UINT32 getLogTimestamp() const{
+        return m_uiLogTimestamp;
+    }
+    inline CWX_UINT32 getItemSize() const{
+        return m_uiItemSize;
+    }
+    inline CWX_UINT32 getDataSize() const{
+        return m_uiDataSize;
+    }
+    inline char const* getData() const{
+        return m_szLogData;
+    }
+    bool operator < (CwxMcQueueItem const& item){
+        if (m_uiLogTimestamp < item.m_uiLogTimestamp) return true;
+        if (m_uiLogTimestamp > item.m_uiLogTimestamp) return false;
+        if (m_uiLogHostId < item.m_uiLogHostId) return true;
+        if (m_uiLogHostId > item.m_uiLogHostId) return false;
+        return m_ullLogSid < item.m_ullLogSid;
+    }
+    ///创建对象
+    static CwxMcQueueItem* createItem(CWX_UINT64 ullSid,
+        CWX_UINT32 uiLogHostId, 
+        CWX_UINT32 uiLogTimestamp,
+        char const* szData,
+        CWX_UINT32 uiDataLen);
+    static void destoryItem(CwxMcQueueItem* item);
+private:
+    CwxMcQueueItem(){};
+    ~CwxMcQueueItem(){}
+private:
+    CWX_UINT64     m_ullLogSid;     ///<日志的sid
+    CWX_UINT32     m_uiLogHostId; ///<日志所属的host
+    CWX_UINT32     m_uiLogTimestamp;  ///<日志的时间戳
+    CWX_UINT32     m_uiItemSize;     ///<item对象的空间大小
+    CWX_UINT32     m_uiDataSize;    ///<data的数据大小
+    char           m_szLogData[0];     ///<data的数据
+};
+
+
+/* 此对象为一个queue对象，管理最新的日志数据。
+**/
+class CwxMcQueue {
+public:
+    CwxMcQueue(CWX_UINT32  uiTimeout, ///< 数据的超时时间
+        CWX_UINT64   ullMaxSize  ///<管理数据的数量
+        )
+    {
+        m_uiTimeout = uiTimeout;
+        m_ullMaxSize = ullMaxSize;
+        m_ullCurSize = 0;
+        m_ullDiscardNum = 0;
+    }
+
+    ~CwxMcQueue() {
+    }
+public:
+    ///添加数据
+    void push(CwxMcQueueItem* log);
+    ///pop数据，空表示没有数据
+    CwxMcQueueItem* pop();
+    ///检查超时
+    void checkTimeout(CWX_UINT32 uiTime);
+    ///获取日志的最大时间戳
+    inline CWX_UINT32 getMaxTimestamp() {
+        CwxMutexGuard<CwxMutexLock>  lock(&m_lock);
+        if (m_queue.size()){
+            CwxMcQueueItem* item = *m_queue.rbegin();
+            return item->m_uiLogTimestamp;
+        }
+        return time(NULL);
+    }
+    ///获取日志的最小时间戳
+    inline CWX_UINT32 getMinTimestamp() {
+        CwxMutexGuard<CwxMutexLock>  lock(&m_lock);
+        if (m_queue.size()){
+            CwxMcQueueItem* item = *m_queue.begin();
+            return item->m_uiLogTimestamp;
+        }
+        return time(NULL);
+    }
+    ///获取日志的数量
+    inline CWX_UINT32 getCount() {
+        CwxMutexGuard<CwxMutexLock>  lock(&m_lock);
+        return m_queue.size();
+    }
+    ///获取空间大小
+    inline CWX_UINT64 getSize() const{
+        return m_ullCurSize
+    }
+    ///获取丢弃的日志数量
+    inline CWX_UINT64 getDiscardNum() {
+        CwxMutexGuard<CwxMutexLock>  lock(&m_lock);
+        return m_ullDiscardNum;
+    }
+private:
+    CwxMutexLock               m_lock;
+    set<CwxMcQueueItem*, CwxPointLess<CwxMcQueueItem> >  m_queue;
+    CWX_UINT32                 m_uiTimeout; ///< 数据的超时时间
+    CWX_UINT64                 m_ullMaxSize;  ///<管理数据的数量
+    CWX_UINT64                 m_ullCurSize;  ///<当前的大小
+    CWX_UINT64                 m_ullDiscardNum; ///<丢弃的日志数量
+};
+
+#endif
